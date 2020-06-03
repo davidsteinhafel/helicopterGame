2686c3954ae12196fb7074f3d37640c6e28ee6f6
David steinhafel <davidsteinhafel@outlook.com>
2020-06-03 12:28:37 -0500

    integrated self made pixel art sprites for helicopter and dead helicopter. scripts allow helicopter/player to click to move vertically and drop on no click. added mine sprites and duplicated. Added collision trigger events so dead heli sprite displays on collision with mine

---
+++ Assets/Resources/Script/Mine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        Debug.Log("hi");
+        GameObject.Find("Player").GetComponent<Player>().isDead = true;
+
+    }
+}
---
+++ Assets/Resources/Script/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Sprite dead1;
+    public bool isDead = false;
+    void FixedUpdate()
+    {
+        float horizontal = 2;
+        float vertical;
+        if (Input.GetMouseButton(0) && isDead == false)
+        {
+            vertical = 5;
+
+        }
+        else
+        {
+            vertical = -5;
+        }
+        
+        Vector3 movement = new Vector3(horizontal, vertical, .5f);
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if(isDead == true)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = dead1;
+            
+        }
+    }
+}
